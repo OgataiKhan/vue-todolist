763c092479ca6931cb14cbc02d2721375d03ebdc
OgataiKhan <badellino.kevin@gmail.com>
2023-11-28 16:16:47 +0100

    fix: formatting

--- js/main.js
+++ js/main.js
@@ -2,41 +2,41 @@
 
 const { createApp } = Vue;
 
-  createApp({
-    data() {
-      return {
-        newTask: '',
-        tasks: [
-          {
-            text: 'Read a book',
-            done: false
-          },
-          {
-            text: 'Pet the cat',
-            done: false
-          },
-          {
-            text: 'Learn to code',
-            done: true
-          },
-        ]
+createApp({
+  data() {
+    return {
+      newTask: '',
+      tasks: [
+        {
+          text: 'Read a book',
+          done: false
+        },
+        {
+          text: 'Pet the cat',
+          done: false
+        },
+        {
+          text: 'Learn to code',
+          done: true
+        },
+      ]
+    };
+  },
+  methods: {
+    deleteTask(index) {
+      this.tasks.splice(index, 1);
+    },
+    addTask() {
+      if (this.newTask.trim() !== '') {
+        this.tasks.push({
+          text: this.newTask,
+          done: false
+        });
+        this.newTask = '';
       }
     },
-    methods: {
-      deleteTask(index) {
-        this.tasks.splice(index, 1);
-      },
-      addTask() {
-        if (this.newTask.trim() !== '') {
-          this.tasks.push({
-            text: this.newTask,
-            done: false
-          });
-          this.newTask = '';
-        }
-      },
-      toggleDone(index) {
-        this.tasks[index].done = !this.tasks[index].done;
-      }
+    toggleDone(index) {
+      this.tasks[index].done = !this.tasks[index].done;
     }
-  }).mount('#app');
+  }
+}).mount('#app');
